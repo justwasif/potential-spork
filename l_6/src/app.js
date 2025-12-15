8f908696e12a37203779bb5abd251d43bfd83035
just_wasif <wasif.iitr.ansari@gmail.com>
2025-12-15 22:12:10 +0530

    edit

--- l_6/src/app.js
+++ l_6/src/app.js
@@ -14,3 +14,7 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use
 export{app}
+
+import useRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/user",userRouter)
